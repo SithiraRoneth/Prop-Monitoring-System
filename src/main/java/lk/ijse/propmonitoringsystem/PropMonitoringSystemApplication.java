05b2a6ec3b6eafb118d0a5517e8218f9f2340b4b
USER <sithiraroneth@gmail.com>
2024-10-27 01:07:32 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/propmonitoringsystem/PropMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.propmonitoringsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropMonitoringSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropMonitoringSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/propmonitoringsystem/PropMonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.propmonitoringsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropMonitoringSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
