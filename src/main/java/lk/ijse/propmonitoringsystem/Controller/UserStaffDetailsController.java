5fc981cd8ceb9a4c3b364c1380e28a860151e231
USER <sithiraroneth@gmail.com>
2024-11-26 11:55:15 +0530

    update UserStaffDetailsController

---
+++ src/main/java/lk/ijse/propmonitoringsystem/Controller/UserStaffDetailsController.java
@@ -0,0 +1,50 @@
+package lk.ijse.propmonitoringsystem.Controller;
+
+import lk.ijse.propmonitoringsystem.Service.UserStaffDetailsService;
+import lk.ijse.propmonitoringsystem.dto.impl.UserStaffDetailsDto;
+import lk.ijse.propmonitoringsystem.exception.DataPersistException;
+import lk.ijse.propmonitoringsystem.util.AppUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("UserStaff/")
+public class UserStaffDetailsController {
+    @Autowired
+    private UserStaffDetailsService userStaffDetailsService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+    produces = MediaType.APPLICATION_JSON_VALUE)
+
+    public ResponseEntity<Void> saveUserStaffDetails(
+            @RequestPart("userStaffId")String userStaffId,
+            @RequestPart("userId") String userId,
+            @RequestPart("staffId") String staffId,
+            @RequestPart("Role") String role
+    )
+
+    {
+        try {
+            //StaffFieldId generate
+//            String userStaffCode = AppUtil.generateUserStaffId();
+
+            //Build the Object
+            UserStaffDetailsDto buildUserStaffDetailsDto = new UserStaffDetailsDto();
+            buildUserStaffDetailsDto.setUserStaffId(userStaffId);
+            buildUserStaffDetailsDto.setUserId(userId);
+            buildUserStaffDetailsDto.setStaffId(staffId);
+            userStaffDetailsService.saveUserStaffDetails(buildUserStaffDetailsDto);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (DataPersistException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
